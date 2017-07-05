3af67fbf7d81247be2498169445ddfcb7590c6f6
TRI VI <trivi2710@gmail.com>
2017-07-04 21:27:03 -0400

    Add event when client type message and send message to the server
    Feat: send message and type message
    
    Send Message: when client click send button, the socket from client
    side will send an event to the server with the message text val from
    input. That message is the broadcasted to all of the other clients.
    
    Type message: (focusing & focus out). When the client start typing
    message, the server will notify the other clients who is typing.

--- public/chatHandle.js
+++ public/chatHandle.js
@@ -22,18 +22,50 @@ socket.on('server-send-logout-success', function(){
 	$('#chatWindow').hide(2000);
 	$('#loginWindow').show(1000);
 });
+
+socket.on('server-send-message-to-all-users', function (data){
+	$('#messageTxt').val("");
+	$('#chatOutputTxt').append('<p><strong>' + data.user + ':</strong> ' + data.message + '</p>');
+	console.log('hhhh');
+});
+
+socket.on('server-send-typing-user', function (data){
+	$('#feedback').html('<p><i>' + data + ' is typing</i></p>')
+});
+
+socket.on('server-send-stop-typing', function (data){
+	$('#feedback').html('');
+});
 	
 $(document).ready(function(){
 	//window.alert('hello');
 	$('#loginWindow').show();
 	$('#chatWindow').hide();
 
+	// registration
 	$('#registerBtn').click(function(){
 		socket.emit('user-send-registration', $('#usernameTxt').val());
 	});
 
-	//logout
+	// logout
 	$('#logoutBtn').click(function() {
 		socket.emit('user-send-logout-request', $('#currentUser').html());
+	});
+
+	// send message
+	$('#messageBtn').click(function() {
+		var messageObj = {
+			user: $('#currentUser').html(),
+			message: $('#messageTxt').val()
+		}
+		socket.emit('user-send-message', messageObj);
+	});
+
+	$('#messageTxt').focusin(function(){
+		socket.emit('user-is-typing', $('#currentUser').html());
+	});
+
+	$('#messageTxt').focusout(function(){
+		socket.emit('user-stop-typing', $('#currentUser').html());
 	});
 });
--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-app.use(express.public('public'));
+app.use(express.static('public'));
 
 var server = require('http').Server(app);
 
 var io = require('socket.io')(server);
 
 // server listen on port 3000 or default port on env
-server.listen(process.env.PORT ||  3000, function() {
+server.listen(3000, function() {
 	console.log('listening on port 3000');
 })
 
@@ -20,8 +18,56 @@ app.get('/', function (req, res) {
 	res.render('index');
 });
 
-io.on('connection', function (socket){
+var userArr = [];
+io.on('connection', function (socket) {
 	console.log('New user connected: ' + socket.id);
 
 	//server is listenning to events
-})
+	socket.on('user-send-registration', function (data){
+		// registration fail - name (data) is already registered
+		if(userArr.indexOf(data) >=0){
+			socket.emit("server-send-registration-fail");
+		} else {
+			//registration success 
+			userArr.push(data);
+			socket.username = data;
+			socket.emit("server-send-registration-success", data);
+
+			//update online users list
+			io.sockets.emit("server-update-userlist", userArr);
+
+		}
+	});
+
+	// server handles logout request
+	socket.on('user-send-logout-request', function (data){
+		console.log("Current User: " + data);
+		var userIndex = userArr.indexOf(data);
+		console.log("Index of user: " + userIndex);
+		userArr.splice(userIndex, 1);
+
+		// server send logout success to that particular user
+		socket.emit('server-send-logout-success');
+
+		// server update online user list
+		io.sockets.emit("server-update-userlist", userArr);
+	});
+
+	// server handle received message
+	socket.on('user-send-message', function (data) {
+		// server send message to everyone's chat output
+		//console.log("Message: " + data.user + ": " + data.message);
+		io.sockets.emit('server-send-message-to-all-users', data);
+		
+	});
+
+	// notify other users that the current user is typing
+	socket.on('user-is-typing', function (data){
+		socket.broadcast.emit('server-send-typing-user', data);
+	});
+
+	socket.on('user-stop-typing', function (data) {
+		console.log('stop-typing');
+		socket.broadcast.emit('server-send-stop-typing', data);
+	});
+});
