56bbdc8033d1c11417de56cc0c1a5c30f7eae0e7
TRI VI <trivi2710@gmail.com>
2017-07-27 15:45:45 -0400

    fix disconnecting bug

--- public/chatHandle.js
+++ public/chatHandle.js
@@ -1,5 +1,5 @@
-//var socket = io.connect('https://tv-messenger.herokuapp.com');
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('https://tv-messenger.herokuapp.com');
+//var socket = io.connect('http://localhost:3000');
 
 socket.on('server-send-registration-fail', function(){
 	window.alert("This name is already registerd. Please choose different name!");
